c59b265b716ae8a8ad84bd9c9b5467b14c0def68
OndrejKocak <ondrej.kocak@gmail.com>
2024-10-24 10:45:05 +0200

    use .env file instead of yaml config to make bot code shorter

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-import yaml
 import discord
 import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
-class Config:
-    file_path = "config.yaml"
-    def __init__(self):
-        with open(self.file_path, "r") as f:
-            self.parser = yaml.safe_load(f)
-    #load google_api_secret
-    def getGoogleAPISecret(self):
-        return self.parser['google_api_secret']
-    #load discord_token
-    def getDiscordToken(self):
-        return self.parser['discord_token']
-    #load target_id
-    def getTargetChannelId(self):
-        return self.parser['target_channel_id']
-    #load mentions option
-    def getMentions(self):
-        return self.parser['mentions']
-    
 class Client(discord.Client):
-    def __init__(self, config, chat, *args, **kwargs):
+    def __init__(self, chat, *args, **kwargs):
         intents = discord.Intents.default()  # Create default intents
         intents.message_content = True
         super().__init__(intents=intents, *args, **kwargs)  # Pass intents to the parent class
-        self.config = config
         self.chat = chat 
     
     async def on_ready(self):
@@ -34,19 +16,17 @@ class Client(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        #short response on ping
-        if (self.config.getMentions() and self.user.mentioned_in(message)):
+        if (os.getenv('MENTIONS').lower() == "true" and self.user.mentioned_in(message)): #short response on ping
             await message.channel.send(self.chat.send_message(message.content.lower(), generation_config = genai.types.GenerationConfig(candidate_count = 1,max_output_tokens = 100, top_p = 0.6, top_k = 5, temperature = 0.6)).text)
         #long response in channel and DMs
-        if (isinstance(message.channel, discord.DMChannel) or message.channel.id == self.config.getTargetChannelId()) and not self.user.mentioned_in(message):
+        if (isinstance(message.channel, discord.DMChannel) or message.channel.id == int(os.getenv('TARGET_CHANNEL_ID'))) and not self.user.mentioned_in(message):
             await message.channel.send(self.chat.send_message(message.content.lower(), generation_config= genai.types.GenerationConfig()).text)   
         
 def main():
-    config = Config()
-    genai.configure(api_key = config.getGoogleAPISecret())
-    chat = genai.GenerativeModel('gemini-pro').start_chat(history=[])
-    client = Client(config, chat)
-    client.run(config.getDiscordToken())
+    load_dotenv()
+    genai.configure(api_key = os.getenv('GOOGLE_API_SECRET'))
+    client = Client(genai.GenerativeModel('gemini-pro').start_chat(history=[]))
+    client.run(os.getenv('DISCORD_TOKEN'))
    
 if __name__ == "__main__":
     main()
